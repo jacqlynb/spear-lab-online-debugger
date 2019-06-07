cdd227c7a12fbc19754eda94e6670e8fe35c8912
jacqlynb <jacqlyn.bender@gmail.com>
2019-06-07 13:07:58 -0400

    create bare-bones page layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = {
+    exceptionData: null,
+    log: []
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    this.fetchData()
+      .then(data => {
+        console.log("first data loggin", data.exception)
+        this.setState({
+          exceptionData: data.exception,
+          log: data.log
+        });
+        console.log("after setting state: ", this.state.exceptionData[0][1].lineNumber)
+        console.log("after setting state", this.state.exceptionData[0][0].lineNumber)
+      }).catch((err) => console.log(err));
+  }
+  
+  render() {
+    let exceptions;
+    console.log(this.state.exceptionData)
+    if (this.state.exceptionData) {
+      exceptions = 
+      <p>{this.state.exceptionData[0][2].lineNumber}</p>
+    } 
+    else {
+      exceptions = null;
+    }
+    return (
+      <div className="app">
+        <div className="header">Working title</div>
+        <div className="container">
+          <div className="log-report">
+            {exceptions}
+          </div>
+          <div className="other">
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  fetchData = async () => {
+    try {
+      const data = await fetch('/hello');
+      return data.json();
+    } catch(error) {
+      console.log('Error in callApi', error);
+    }
+  };
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
