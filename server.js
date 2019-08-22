d43f2da270886d5bbc02e3aad711af2d96663ef4
jacqlynb <jacqlyn.bender@gmail.com>
2019-08-22 17:18:51 -0400

    add scripts for heroku deployment

--- server.js
+++ server.js
@@ -68,6 +68,14 @@ function constructLogHierarchy1(data) {
   return log;
 }
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 // ***** Promise.all pattern *****
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/sample-data', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sample-data', {
   useNewUrlParser: true,
   useCreateIndex: true
 }).catch((error) => {
